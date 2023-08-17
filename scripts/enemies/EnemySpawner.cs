fd52e18123c47021fe8f9a64ea445d0300775e0b
Callum Brezden <callumbrezden@gmail.com>
2023-08-16 21:49:49 -0700

    Added Random Spawning

--- scripts/enemies/EnemySpawner.cs
+++ scripts/enemies/EnemySpawner.cs
@@ -1,22 +1,39 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class EnemySpawner : Node2D
 {
-	private Node DetonixScene;
-	private static Random _random = new Random();
+    private Node DetonixScene;
+    private static Random _random = new Random();
 
-	public override void _Ready()
-	{
-		DetonixScene = ResourceLoader.Load<PackedScene>("res://scenes/enemies/Detonix.tscn").Instantiate();
-		SpawnEnemy(5);
-	}
+    private List<Vector2> spawnPositions = new List<Vector2>
+    {
+        new Vector2(17, 144),
+        new Vector2(144, 17),
+        new Vector2(272, 144),
+        new Vector2(144, 272)
+    };
 
-	public void SpawnEnemy(int enemyAmount){
-		for(int i = 1; i <= enemyAmount; i++){
-			var enemy = DetonixScene.Duplicate() as Detonix;
-			enemy.Position = new Vector2(_random.Next(17, 256), _random.Next(17, 256));
-			AddChild(enemy);
-		}
-	}
+    public override void _Ready()
+    {
+        DetonixScene = ResourceLoader.Load<PackedScene>("res://scenes/enemies/Detonix.tscn").Instantiate();
+        SpawnEnemy(5);
+    }
+
+    public void SpawnEnemy(int enemyAmount)
+    {
+        for (int i = 1; i <= enemyAmount; i++)
+        {
+            var enemy = DetonixScene.Duplicate() as Detonix;
+            enemy.Position = GetRandomPosition();
+            AddChild(enemy);
+        }
+    }
+
+    private Vector2 GetRandomPosition()
+    {
+        int index = _random.Next(spawnPositions.Count);
+        return spawnPositions[index];
+    }
 }
