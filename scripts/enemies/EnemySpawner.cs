1a28bb54ef18a17cf441b512793f34f3d27658b8
Callum Brezden <callumbrezden@gmail.com>
2023-08-17 19:55:12 -0700

    Optimized Enemy Spawner

--- scripts/enemies/EnemySpawner.cs
+++ scripts/enemies/EnemySpawner.cs
@@ -1,39 +1,43 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 public partial class EnemySpawner : Node2D
 {
     private Node DetonixScene;
-    private static Random _random = new Random();
-
-    private List<Vector2> spawnPositions = new List<Vector2>
-    {
-        new Vector2(17, 144),
-        new Vector2(144, 17),
-        new Vector2(272, 144),
-        new Vector2(144, 272)
-    };
+    private static Random random = new Random();
 
     public override void _Ready()
     {
         DetonixScene = ResourceLoader.Load<PackedScene>("res://scenes/enemies/Detonix.tscn").Instantiate();
-        SpawnEnemy(5);
+        SpawnEnemies(15);
     }
 
-    public void SpawnEnemy(int enemyAmount)
+    public void SpawnEnemies(int enemyAmount)
     {
         for (int i = 1; i <= enemyAmount; i++)
         {
+            int randomNumber = random.Next(0, 4);
             var enemy = DetonixScene.Duplicate() as Detonix;
-            enemy.Position = GetRandomPosition();
+
+            switch (randomNumber)
+            {
+                case 0:
+                    enemy.Position = new Vector2((144 + random.Next(-32, 33)), 0);
+                    break;
+                case 1:
+                    enemy.Position = new Vector2((144 + random.Next(-32, 33)), 288);
+                    break;
+                case 2:
+                    enemy.Position = new Vector2(288, (144 + random.Next(-32, 33)));
+                    break;
+                case 3:
+                    enemy.Position = new Vector2(0, (144 + random.Next(-32, 33)));
+                    break;
+            }
+
             AddChild(enemy);
         }
-    }
-
-    private Vector2 GetRandomPosition()
-    {
-        int index = _random.Next(spawnPositions.Count);
-        return spawnPositions[index];
     }
 }
