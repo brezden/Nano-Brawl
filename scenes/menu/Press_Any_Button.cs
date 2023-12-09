10fe863fc65a358719b8b0e6c6cae4d0e1ffe1e3
Callum Brezden <callumbrezden@gmail.com>
2023-12-09 14:14:37 -0800

    Remove .vs directory and .import files from tracking and added Press Start Prompt

--- scenes/menu/Press_Any_Button.cs
+++ scenes/menu/Press_Any_Button.cs
@@ -3,18 +3,13 @@ using System;
 
 public partial class Press_Any_Button : Node
 {
-    private Timer _timer;
-    private float _flashRate = 0.5f; // seconds
+    BoxContainer StartGameContainer;
 
     public override void _Ready() {
-        _timer = new Timer();
-        AddChild(_timer);
-        _timer.WaitTime = _flashRate;
-        _timer.Connect("timeout", this, nameof(OnTimerTimeout));
-        _timer.Start();
+        StartGameContainer = this.GetNode("%StartGameContainer") as BoxContainer;
     }
 
     private void OnTimerTimeout() {
-        Visible = !Visible; // Toggle visibility
+        StartGameContainer.Visible = !StartGameContainer.Visible;
     }
 }
